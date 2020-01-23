81bec9c5648dba9387ab5db46b8be9b1b8445404
Felipe <felipe.almeidaa14@gmail.com>
2020-01-23 10:24:51 -0300

    Criação de enums para setar a configuração do driver manager

---
+++ src/test/java/br/com/rsinet/HUB_BDD/enums/DriverType.java
@@ -0,0 +1,7 @@
+package br.com.rsinet.HUB_BDD.enums;
+
+public enum DriverType {
+	FIREFOX,
+	CHROME,
+	INTERNETEXPLORER
+}
---
+++ src/test/java/br/com/rsinet/HUB_BDD/enums/EnviromentLocal.java
@@ -0,0 +1,6 @@
+package br.com.rsinet.HUB_BDD.enums;
+
+public enum EnviromentLocal {
+	LOCAL, 
+	REMOTE,
+}
